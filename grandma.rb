f82bcf047f28386be50f68b39f0942769e18edbe
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-10 20:40:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
 if
-  #{phrase} == "I LOVE YOU GRANDMA!"
+  phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
 end
 elsif 
-  #{phrase} == phrase.upcase
+  phrase == phrase.upcase
   puts "NO, NOT SINCE 1938!"
 end
 else
